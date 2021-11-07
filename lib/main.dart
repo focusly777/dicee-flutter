9512cd92df4654e3cd9b115edff3d2fe1a9573f6
Focusly Corp <77958705+focusly777@users.noreply.github.com>
2021-11-08 07:56:24 +0900

    diceapp complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -17,32 +19,44 @@ void main() {
 
 class DicePage extends StatefulWidget {
   @override
-  State<DicePage> createState() => _DicePageState();
+  _DicePageState createState() => _DicePageState();
 }
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
-        children: [
+        children: <Widget>[
           Expanded(
             child: FlatButton(
+              child: Image.asset(
+                'images/dice$leftDiceNumber.png',
+              ),
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = 5;
-                });
+                changeDiceFace();
               },
-              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
+          //Get students to create the second die as a challenge
           Expanded(
             child: FlatButton(
+              child: Image.asset(
+                'images/dice$rightDiceNumber.png',
+              ),
               onPressed: () {
-                print('Right Button got pressed.');
+                changeDiceFace();
               },
-              child: Image.asset('images/dice1.png'),
             ),
           ),
         ],
